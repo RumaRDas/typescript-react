0c2338884225cc450d6a085a937f45c17d53282f
Ruma Das <ruma@neo-analytics.com,au>
2023-09-25 00:37:34 +0930

    "Edding file"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+// import pokemon from "./pokemon.json";
+import "./App.css";
+import Button from "@mui/material/Button";
 
-function App() {
+const PokemonRow = ({ pokeman, onSelect }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <tr>
+      <td>{pokeman.name.english}</td>
+      <td>{pokeman.type.join(", ")}</td>
+      <td>
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => onSelect(pokeman)}
         >
-          Learn React
-        </a>
-      </header>
+          Select!
+        </Button>
+      </td>
+    </tr>
+  );
+};
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({ english: PropTypes.string }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  }),
+  onSelect: PropTypes.func,
+};
+const PokemonInfo = ({ name, base }) => {
+  return (
+    <div>
+      <h1>{name.english}</h1>
+      <table>
+        {Object.keys(base).map((
+          key,
+          i // For base all keys
+        ) => (
+          <tbody key={i}>
+            <tr key={key}>
+              <td>{key}</td>
+              {/* Getting value according to keys */}
+              <td>{base[key]}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
     </div>
+  );
+};
+PokemonInfo.propTypes = {
+  name: PropTypes.shape({ english: PropTypes.string.isRequired }),
+  base: PropTypes.shape({
+    HP: PropTypes.number.isRequired,
+    Attack: PropTypes.number.isRequired,
+    Defense: PropTypes.number.isRequired,
+    "Sp. Attack": PropTypes.number.isRequired,
+    "Sp. Defense": PropTypes.number.isRequired,
+    Speed: PropTypes.number.isRequired,
+  }),
+};
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 70% 30%;
+  grid-column-gap: 1rem;
+`;
+
+const Container = styled.div`
+  margin: auto;
+  width: 800px;
+  padding-top: 1rem;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`;
+function App() {
+  const [filter, setFilter] = useState("");
+  const [pokemon, setPokemon] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/typescript-react/pokemon.json")
+      .then((res) => res.json())
+      .then((resData) => setPokemon(resData));
+  }, []);
+
+  return (
+    <Container>
+      <Title>Pokemon Search</Title>
+
+      <TwoColumnLayout>
+        <div>
+          <Input value={filter} onChange={(e) => setFilter(e.target.value)} />
+          <table width="100%">
+            <thead>
+              <tr>
+                <th> Name</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon
+                // {      serching the pokemon}
+                .filter((d) =>
+                  d.name.english.toLowerCase().includes(filter.toLowerCase())
+                )
+                // Getting 20 value from all array
+                .slice(0, 20)
+                .map((pokeman) => {
+                  return (
+                    <PokemonRow
+                      pokeman={pokeman}
+                      key={pokeman.id}
+                      onSelect={(pokeman) => setSelectedItem(pokeman)}
+                    />
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+        {selectedItem && <PokemonInfo {...selectedItem} />}
+      </TwoColumnLayout>
+    </Container>
   );
 }
 
